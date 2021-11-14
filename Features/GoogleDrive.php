1c7ca762486a1c4a51ef61eedee258502ee983ca
Hao Hoang <38025803+haohoangofficial@users.noreply.github.com>
2021-11-14 18:00:15 +0700

    Embedding a Google Drive folder in Wordpress.com

---
+++ Features/GoogleDrive.php
@@ -0,0 +1,4 @@
+/**
+* Embedding a Google Drive folder in Wordpress.com
+*/
+<iframe src="https://drive.google.com/embeddedfolderview?id=Folder_ID#grid" frameborder="0" width="640" height="480" marginheight="0" marginwidth="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
